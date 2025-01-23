ad0b5723c9b6e356ea4acf02cab048d0d31755f7
brianlabarbera <74026911+brianlabarbera@users.noreply.github.com>
2025-01-23 16:27:28 -0500

    Time: 1 ms (60.53%), Space: 19 MB (23.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # initialize hashmap
         hashmap = {}
+        #iterate through array...len() calculates the length of the list and range() is like i++
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashmap:
