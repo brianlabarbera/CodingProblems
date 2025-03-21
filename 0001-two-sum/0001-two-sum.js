3a906271f186318a68b6467afd6f1fbeb5754454
brianlabarbera <74026911+brianlabarbera@users.noreply.github.com>
2025-03-21 19:35:33 -0400

    Time: 2 ms (74.14%), Space: 54.4 MB (34.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,19 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) { 
+var twoSum = function(nums, target) {
 
-    //Initialize hashmap
-    let hashmap = new Map()
-    //Iterate through array and add complements to hashmap
-    for (let i = 0; i <= nums.length; i++) {
-        //Initialize complement and define
-        let complement = target - nums[i]
-        //Check if complement is in hashmap and if true, return answer
-        if (hashmap.has(complement)) {
-            return [i, hashmap.get(complement)]
-        }
-        //If false, complement is not present, add to hashmap
-        hashmap.set(nums[i], i)
+    let hashmap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+
+     let complement = target - nums[i];
+
+     if (hashmap.has(complement)) {
+         return [hashmap.get(complement), i]
+     }
+     
+     hashmap.set(nums[i], i);
+        
+
     }
+    
+    return [];
 };
